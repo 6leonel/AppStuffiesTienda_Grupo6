281f784ea7cbb12424cad5e02e5c99fcc4570bb7
6smoke <LEON.PAVEZ@DUOCUC.CL>
2025-09-12 22:19:27 -0300

    a

--- app/src/main/java/com/example/appstuffiestienda_grupo6/ProductoRepository.kt
+++ app/src/main/java/com/example/appstuffiestienda_grupo6/ProductoRepository.kt
@@ -5,9 +5,10 @@ import com.example.appstuffiestienda_grupo6.model.Producto
 class ProductoRepository {
     fun getProductos(): List<Producto> {
         return listOf(
-            Producto(1, "Polera Oversize", 12990.0),
-            Producto(2, "Polerón Stuffies", 19990.0),
-            Producto(3, "Gorro Urbano", 7990.0)
+            Producto(1, "Leonel Pavez", .0),
+            Producto(2, "Miguel Torres", .0),
+            Producto(3, "Alexis Olguin", .0),
+            Producto(3, "Alfonso David ", .0)
         )
     }
 }
